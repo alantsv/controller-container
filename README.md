8089f56d1e6e9eb3da903581048b3dedf3869f02
Alan Veloso <alantsv@gercom>
2017-06-29 14:41:43 -0300

    README complemented

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
-# Controladores em Containers
+Controladores Embutidos Containers
+==================================
 
-## Docker
+## Controlador
 
-POX
+### Floodlight
 
-Floodlight
+[Floodlight Web Site](http://www.projectfloodlight.org/floodlight/)
+
+## Conteiners
+
+### LXC
+
+[LXC Web Site](https://linuxcontainers.org/lxc/introduction/)
+
+### Docker
+
+[Docker Web Site](https://www.docker.com/)
+
+### LXD
+
+[LXD Web Site](https://linuxcontainers.org/lxd/introduction/)
+
+### OpenVZ
+
+[OpenVZ Wiki](https://openvz.org/Main_Page)
+
+### lmctfy
+
+[lmctfy Repositório no Github](https://github.com/google/lmctfy)
+
+## Objetivo
+
+## Como funciona?
+
+Docker:
+
+  `sudo docker run -p 8080:8080 -p 6653:6653 floodlight-docker`
+
+Topologia:
+
+  `sudo mn --custom controller-container\topo-Nsw-2host.py --topo lineartopo --controller=remote,ip=<ip-container>,port=6653`
--- floodlight-lxc/floodlight-lxc.py
+++ floodlight-lxc/floodlight-lxc.py
@@ -11,20 +11,36 @@ if not os.geteuid() == 0:
 # Setup the container object
 c = lxc.Container("floodlight-lxc")
 if not c.defined:
-    c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "debian",
-                                                   "release": "jessie",
+    c.create("download", lxc.LXC_CREATE_QUIET, {"dist": "ubuntu",
+                                                   "release": "xenial",
                                                    "arch": "amd64"})
     c.start()
     c.get_ips(timeout=30)
     c.attach_wait(lxc.attach_run_command, ["apt-get", "update"])
-    c.attach_wait(lxc.attach_run_command, ["apt-get", "dist-upgrade", "-y"])
+    # c.attach_wait(lxc.attach_run_command, ["apt-get", "dist-upgrade", "-y"])
+    # c.attach_wait(lxc.attach_run_command, ["apt-get", "install", "software-properties-common", "-y", "--force-yes"])
+    c.attach_wait(lxc.attach_run_command, ["add-apt-repository", "ppa:webupd8team/java", "-y"])
+    c.attach_wait(lxc.attach_run_command, ["apt-get", "update"])
+    # c.attach_wait(lxc.attach_run_command, ["apt-get", "install", "oracle-java8-set-default", "-y"])
+    # c.attach_wait(lxc.attach_run_command, ["apt-get", "install",
+    #                                          "build-essential", "ant", "maven", "python-dev", "git","--force-yes"])
+    
+    # c.attach_wait(lxc.attach_run_command, ["git", "clone", "git://github.com/floodlight/floodlight.git"])
+    # c.attach_wait(lxc.attach_run_command, ["cd", "floodlight/"])
+    # c.attach_wait(lxc.attach_run_command, ["git", "submodule", "init"])
+    # c.attach_wait(lxc.attach_run_command, ["git", "submodule", "update"])
+    # c.attach_wait(lxc.attach_run_command, ["ant"])
+    
+    # c.attach_wait(lxc.attach_run_command, ["mkdir", "-p", "/var/lib/floodlight"])
+    # c.attach_wait(lxc.attach_run_command, ["chmod", "777", "/var/lib/floodlight"])
+    
+    # c.attach_wait(lxc.attach_run_command, ["java", "-jar", "target/floodlight.jar"])
+
+
 
     c.start()
     c.get_ips(timeout=30)
-    c.attach_wait(lxc.attach_run_command, ["apt-key", "adv", "--keyserver", "hkp://keyserver.ubuntu.com:80", "--recv-keys", "EEA14886"])
-    c.attach_wait(lxc.attach_run_command, ["apt-get", "update"])
-    c.attach_wait(lxc.attach_run_command, ["apt-get", "install",
-                                             "build-essential", "ant", "mave", "python-dev", "oracle-java8-installer" , "git", "-y"])
+
 
 if not c.start():
     print("Failed to start the container", file=sys.stderr)
